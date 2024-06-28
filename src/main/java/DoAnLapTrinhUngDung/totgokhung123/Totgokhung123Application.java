ce95c4ae3ca22598afe40beada187f989527d09b
totgokhung123 <chutienbinh2003@gmail.com>
2024-06-28 21:10:48 +0700

    Dang test giao dien

---
+++ src/main/java/DoAnLapTrinhUngDung/totgokhung123/Totgokhung123Application.java
@@ -0,0 +1,13 @@
+package DoAnLapTrinhUngDung.totgokhung123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Totgokhung123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Totgokhung123Application.class, args);
+	}
+
+}
---
+++ src/test/java/DoAnLapTrinhUngDung/totgokhung123/Totgokhung123ApplicationTests.java
@@ -0,0 +1,13 @@
+package DoAnLapTrinhUngDung.totgokhung123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Totgokhung123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
